9ee53d153ae57656bfe52a186d7daccfb8ef8c81
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-01-26 18:17:54 +0100

    updating humidity and wind

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
-
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.main.wind);
 }
 let apiKey = "1e7e4fc41dea029f49d7f03e46e28df4";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
